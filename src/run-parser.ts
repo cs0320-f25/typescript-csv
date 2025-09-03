a7242726ddc119a60107c08834e7192943d61d11
Tim Nelson <tbnelson@gmail.com>
2025-09-03 16:33:13 -0400

    fixes: gitignore/node_modules, remove generator

--- src/run-parser.ts
+++ src/run-parser.ts
@@ -1,13 +1,21 @@
-import { parseCSVStream } from "./basic-parser";
+import { parseCSV } from "./basic-parser";
 
-const DATA_FILE = "../data/people.csv"; // update with your actual file name
+/*
+  Example of how to run the parser outside of a test suite.
+*/
+
+const DATA_FILE = "./data/people.csv"; // update with your actual file name
 
 async function main() {
-  for await (const record of parseCSVStream(DATA_FILE)) {
-    console.log(record);
-  }
+  // Because the parseCSV function needs to "await" data, we need to do the same here.
+  const results = await parseCSV(DATA_FILE)
+
+  // Notice the difference between "of" and "in". One iterates over the entries, 
+  // another iterates over the indexes only.
+  for(const record of results)
+    console.log(record)
+  for(const record in results)
+    console.log(record)
 }
 
 main();
-
-// run with npx ts-node run-parser.ts
--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -1,13 +1,11 @@
-import { parseCSVStream } from "../src/basic-parser";
+import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
 
-test("parseCSVStream yields arrays", async () => {
-  const results = [];
-  for await (const row of parseCSVStream(PEOPLE_CSV_PATH)) {
-    results.push(row);
-  }
+test("parseCSV yields arrays", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+  
   expect(results).toHaveLength(5);
   expect(results[0]).toEqual(["name", "age"]);
   expect(results[1]).toEqual(["Alice", "23"]);
@@ -16,8 +14,9 @@ test("parseCSVStream yields arrays", async () => {
   expect(results[4]).toEqual(["Nim", "22"]);
 });
 
-test("parseCSVStream yields only arrays", async () => {
-  for await (const row of parseCSVStream(PEOPLE_CSV_PATH)) {
+test("parseCSV yields only arrays", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+  for(const row of results) {
     expect(Array.isArray(row)).toBe(true);
   }
 });
